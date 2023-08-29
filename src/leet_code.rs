211b6e616f7e2db9682c4f7676702743768e06fd
Schaitanya535 <seschaitanya535@gmail.com>
2023-08-29 23:57:57 +0530

    best_closing_time_answer

--- src/leet_code.rs
+++ src/leet_code.rs
@@ -1,2 +1,12 @@
+pub mod best_closing_time;
 pub mod is_palindrome;
 pub mod two_sum;
+
+pub fn leet_code() {
+    use best_closing_time::best_closing_time;
+    use is_palindrome::is_palindrome;
+    use two_sum::two_sum;
+    two_sum();
+    is_palindrome();
+    best_closing_time();
+}
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,9 @@ mod leet_code;
 
 #[tokio::main]
 async fn main() {
-    use leet_code::is_palindrome::is_palindrome;
-    use leet_code::two_sum::two_sum;
-    two_sum();
-    is_palindrome();
+    use leet_code::leet_code;
+    leet_code();
+
     // let app = Router::new().route("/", get(|| async { "Hello, World!" }));
     // let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     // axum::Server::bind(&addr)
