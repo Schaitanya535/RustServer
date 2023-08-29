d652803dd53ecb7ee7242d0d2d616badef8320ba
Schaitanya535 <seschaitanya535@gmail.com>
2023-08-29 23:12:15 +0530

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,12 @@
-use axum::{routing::get, Router};
-use std::net::SocketAddr;
+// use axum::{routing::get, Router};
+// use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-
-    // run it with hyper on localhost:3000
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-    // checking if I can commit to this repo.
+    // let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    // let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    // axum::Server::bind(&addr)
+    //     .serve(app.into_make_service())
+    //     .await
+    //     .unwrap();
 }
