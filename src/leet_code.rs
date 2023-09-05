bd8ff2112f206b8ec13dbba9acbc128d7799dbaa
Schaitanya535 <seschaitanya535@gmail.com>
2023-09-06 02:04:53 +0530

    Min partitions question solved with two answers

--- src/leet_code.rs
+++ src/leet_code.rs
@@ -1,6 +1,7 @@
 pub mod avg_salary;
 pub mod best_closing_time;
 pub mod counting_bits;
+pub mod deci_binary_partition;
 pub mod extra_characters_in_string;
 pub mod height_balanced_bst;
 pub mod insert_gcd_linked_list;
@@ -18,4 +19,5 @@ pub fn main() {
     avg_salary::solve();
     height_balanced_bst::solve();
     unique_paths::solve();
+    deci_binary_partition::solve()
 }
---
+++ src/leet_code/deci_binary_partition.rs
@@ -0,0 +1,20 @@
+pub fn solve() {
+    // let ans = min_partitions("12346668".to_owned());
+    let ans = min_partitions_2("12346668".to_owned());
+    println!("min num of partitions {}", ans);
+}
+
+pub fn min_partitions(n: String) -> i32 {
+    const RADIX: u32 = 10;
+    let mut max = '0';
+    for i in n.chars() {
+        if i.to_digit(RADIX).unwrap() > max.to_digit(RADIX).unwrap() {
+            max = i;
+        }
+    }
+    return max.to_digit(RADIX).unwrap() as i32;
+}
+
+pub fn min_partitions_2(n: String) -> i32 {
+    n.chars().max().unwrap() as i32 - '0' as i32
+}
