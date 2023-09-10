dd9e624b11455f8cd9b575a393a067c64c4f140d
Schaitanya535 <seschaitanya535@gmail.com>
2023-09-10 17:10:10 +0530

    Count orders minor refactoring

--- src/leet_code/count_order.rs
+++ src/leet_code/count_order.rs
@@ -5,8 +5,7 @@ pub fn solve() {
 
 pub fn count_orders(n: i32) -> i32 {
     const IE7: i64 = 1000_000_000 + 7;
-    (1..=n).fold(1 as i64, |mut acc: i64, i| {
-        acc = ((acc * (i as i64) % IE7) * (2 * i - 1) as i64) % IE7;
-        acc
+    (1..=n).fold(1 as i64, |acc: i64, i| {
+        ((acc * (i as i64) % IE7) * (2 * i - 1) as i64) % IE7
     }) as i32
 }
