f1f54ecfe118ecef8008228fe4e94a2c5eb36004
Schaitanya535 <seschaitanya535@gmail.com>
2023-09-16 15:47:26 +0530

    Added Union Find Utils

--- src/utils.rs
+++ src/utils.rs
@@ -29,3 +29,41 @@ pub fn pow_mod(base: i32, exp: i32) -> i32 {
 pub fn factorial(n: i32) -> i32 {
     (1..=(n as i64)).fold(1 as i64, |acc: i64, i| (acc * i) % MOD) as i32
 }
+
+pub struct UnionFind {
+    parent: Vec<usize>,
+    rank: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+            rank: vec![0; n],
+        }
+    }
+
+    fn find(&mut self, u: usize) -> usize {
+        if self.parent[u] == u {
+            return u;
+        }
+        self.parent[u] = self.find(self.parent[u]);
+        self.parent[u]
+    }
+
+    fn union(&mut self, mut u: usize, mut v: usize) -> bool {
+        u = self.find(u);
+        v = self.find(v);
+        if u == v {
+            return false;
+        }
+        if self.rank[u] > self.rank[v] {
+            std::mem::swap(&mut u, &mut v);
+        }
+        self.parent[u] = v;
+        if self.rank[u] == self.rank[v] {
+            self.rank[v] += 1;
+        }
+        true
+    }
+}
