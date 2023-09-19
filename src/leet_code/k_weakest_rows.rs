6f9d23b7d1ed1296179b74ce570062f9fd11f740
Schaitanya535 <seschaitanya535@gmail.com>
2023-09-19 13:16:16 +0530

    Can use default sort.

--- src/leet_code/k_weakest_rows.rs
+++ src/leet_code/k_weakest_rows.rs
@@ -1,4 +1,4 @@
-use std::cmp::Ordering;
+// use std::cmp::Ordering;
 
 pub fn solve() {
     let matrix = vec![vec![1, 1, 0], vec![0, 0, 0], vec![1, 0, 0]];
@@ -10,13 +10,14 @@ pub fn k_weakest_rows(matrix: Vec<Vec<i32>>, k: i32) -> Vec<i32> {
     let mut list: Vec<(i32, i32)> = matrix
         .iter()
         .enumerate()
-        .map(|(i, row)| (i as i32, row.iter().sum()))
+        .map(|(i, row)| (row.iter().sum(), i as i32))
         .collect();
 
-    list.sort_by(|a, b| match a.1.cmp(&b.1) {
-        Ordering::Equal => a.0.cmp(&b.0),
-        other => other,
-    });
+    // list.sort_by(|a, b| match a.0.cmp(&b.0) {
+    //     Ordering::Equal => a.1.cmp(&b.1),
+    //     other => other,
+    // });
 
+    list.sort();
     list.iter().map(|x| x.0).take(k as usize).collect()
 }
