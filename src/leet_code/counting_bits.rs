f96175ffb09bd179c86f12ca4d32f558a145c2a8
ChaitanyaS <chaitanya.s@pei.group>
2025-03-20 01:26:47 +0530

    solved leet code question

--- src/leet_code/counting_bits.rs
+++ src/leet_code/counting_bits.rs
@@ -6,6 +6,37 @@ fn count_bits(n: i32) -> Vec<i32> {
     return ans;
 }
 
+pub fn all_ones(nums: Vec<i32>) -> bool {
+    nums.iter().all(|&x| x == 1)
+}
+
+pub fn min_operations(mut nums: Vec<i32>) -> i32 {
+    let mut ans = 0;
+
+    if nums.len() < 3 {
+        if all_ones(nums) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    for i in 0..nums.len() - 2 {
+        if nums[i] == 0 {
+            nums[i] = 1;
+            nums[i + 1] = 1 - nums[i + 1];
+            nums[i + 2] = 1 - nums[i + 2];
+            ans += 1;
+        }
+    }
+
+    if all_ones(nums) {
+        return ans;
+    } else {
+        return -1;
+    }
+}
+
 pub fn solve() {
     count_bits(30);
     // ans.iter().for_each(|i| print!("{}", i));
